e5ba2cb9ca7f6edac1c4a04b6d8d1f2a21632eac
Chris Davila <cmd1603@gmail.com>
2019-08-12 15:14:05 -0500

    Added posts resource controller for CRUD operations on blog posts and roll-dice exercise

---
+++ resources/views/rolldice.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Random Dice Roll</title>
+</head>
+<body>
+    <h1>Guess is: <?php echo $guess; ?></h1>
+    <h1>Random number is: <?php echo $random_number; ?></h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sayhello/{name}', function($name)
+{
+    if ($name == "Chris") {
+        return Redirect::to('/');
+    }
+
+    return view('my-first-view');
+});
+
+Route::get('/saydice/{guess}', function($guess)
+{
+	$random_number = rand(1, 6);
+	$data = array(
+		'guess' => $guess,
+	  	'random_number' => $random_number
+	);
+
+	return view('rolldice')->with($data);
+});
+
+Route::resource('posts', 'PostsController');
