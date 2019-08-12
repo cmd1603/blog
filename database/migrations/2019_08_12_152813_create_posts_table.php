e4e00e3087b244d062b6baaa2efb9ce0560533f5
Chris Davila <cmd1603@gmail.com>
2019-08-12 12:07:13 -0500

    Created database tables and migrations

---
+++ database/migrations/2019_08_12_152813_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 50);
+            $table->string('img_path')->nullable();
+            $table->string('url');
+            $table->text('content');
+            $table->integer('created_by')->unsigned();
+            $table->foreign('created_by')->references('id')->on('users');                       
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
